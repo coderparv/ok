1a29db86cd7c926340aaaf411dda4b27ca931837
its parv <90262471+coderparv@users.noreply.github.com>
2022-01-10 22:43:38 +0530

    Update alive.py

--- RiZoeLXSpam/plugins/alive.py
+++ RiZoeLXSpam/plugins/alive.py
@@ -1,41 +1,31 @@
 from .. import Riz, SUDO_USERS, rizoelversion
 from .. import ALIVE_PIC
-from telethon import events, version, Button
-from telethon.tl.custom import button
+from telethon import events, version
 from time import time
 from datetime import datetime
 
-RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
+RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/572a9c973dc1965a6b46c.png"
   
 
           
-rizoel = "✧ 𝑅𝐼𝑍𝑂𝐸𝐿 𝑋 𝑆𝑃𝐴𝑀 𝐼𝑍𝑍 𝐴𝐿𝐼𝑉𝐸𝐸 ✧\n\n"
+rizoel = " DIVINE SPAM❤️‍🔥 ✧\n\n"
 
 rizoel += f"┏━━━━━━━━━━━━━━━━━━━\n"
 
 rizoel += f"┣➣ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
+ 
+rizoel += f"┣➣ **sᴜᴘᴘᴏʀᴛ** : [JOIN](https://t.me/centraI_community)\n"
 
-rizoel += f"┣➣ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
-
-rizoel += f"┣➣ **ʀɪᴢᴏᴇʟXsᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{rizoelversion}`\n"
+rizoel += f"┣➣ **ᴄʜᴀɴɴᴇʟ** : [JOIN](https://t.me/avalivefru)\n"
 
 rizoel += f"┗━━━━━━━━━━━━━━━━━━━\n\n"
-         
+
+rizoel += f"🖤 [𝐑𝐄𝐏𝐎](https://t.me/parvcodes) 🖤"            
                                     
-@Riz.on(events.NewMessage(pattern=".alive"))
+@Riz.on(events.NewMessage(pattern=r"\.alive"))
 async def alive(event):
     if event.sender_id in SUDO_USERS:
      await Riz.send_file(event.chat_id,
                                   RIZ_PIC,
-                                  caption=rizoel,
-                                  buttons=[
-        [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/RiZoeLX"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/DNHxHELL")
-        ],
-        [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/MrRizoel/RiZoeLXSpam")
-        ]
-        ]
-        )
+                                  caption=rizoel)
     
